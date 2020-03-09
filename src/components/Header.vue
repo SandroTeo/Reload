56122c3691aabe18ce99eb7b71980a92455ec37a
SandroTeo <sandroteo@gmail.com>
2020-03-09 19:57:49 -0300

    comit inicial

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="header">
+        <header class="header">
+          <div>
+             <img src="" alt="">   
+          </div>
+          <div>
+            <nav>
+                <div>
+                   <router-link> </router-link>
+                   <router-link> </router-link>
+                   <router-link> </router-link>                  
+                </div> 
+            </nav>  
+          </div>
+        </header>
+    </div>
+</template>
